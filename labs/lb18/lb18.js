13de91ed6b6c0f303a318ebed7cde66e5c2e3ec5
Syeda Samantha <>
2022-05-17 16:52:22 +0000

    Initial commit

---
+++ labs/lb18/lb18.js
@@ -0,0 +1,15 @@
+function size(){
+  document.getElementById("movingBox").style.="";
+}
+
+function move(){
+  document.getElementById("movingBox").style.="";
+}
+
+function circle(){
+  document.getElementById("movingBox").style.="";
+}
+
+function hue(){
+  document.getElementById("movingBox").style.background-color="green";
+}
